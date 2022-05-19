deda0889c0548341df7f5c4485eb1180ecd2031e
James Long <longster@gmail.com>
2022-05-19 12:52:04 -0400

    Update due to open-sourcing

--- src/index.js
+++ src/index.js
@@ -33,62 +33,65 @@ async function setupHandlers() {
   });
 }
 
-let actualRoamAuth = {
-  graph: process.env.ACTUAL_ROAM_API_GRAPH,
-  email: process.env.ACTUAL_ROAM_API_EMAIL,
-  password: process.env.ACTUAL_ROAM_API_PASSWORD
-};
-
-let githubBotAuth = {
-  appId: 171985,
-  privateKey: process.env.ACTUAL_BOT_PRIVATE_KEY,
-  installationId: 23217937
-};
+// let githubBotAuth = {
+//   appId: 171985,
+//   privateKey: process.env.ACTUAL_BOT_PRIVATE_KEY,
+//   installationId: 23217937
+// };
 
 const flows = {
-  'Discord Actual reactions': require('./flows/discord-reaction-roam')({
-    discord: config.discordActual,
-    roamAuth: actualRoamAuth
-  }),
-  'Feeds to discord (release)': require('./flows/atom-publish')({
-    discord: config.discordActual,
-    channel: '940289937754951730',
-    url: 'https://actualbudget.com/blog/feed.xml?filter=release'
-  }),
-  'Feeds to discord (all)': require('./flows/atom-publish')({
-    discord: config.discordActual,
-    channel: '940290142579605514',
-    url: 'https://actualbudget.com/blog/feed.xml'
-  }),
+  // 'Discord Actual reactions': require('./flows/discord-reaction-roam')({
+  //   discord: config.discordActual,
+  //   roamAuth: actualRoamAuth
+  // }),
+  // 'Feeds to discord (release)': require('./flows/atom-publish')({
+  //   discord: config.discordActual,
+  //   channel: '940289937754951730',
+  //   url: 'https://actualbudget.com/blog/feed.xml?filter=release'
+  // }),
+  // 'Feeds to discord (all)': require('./flows/atom-publish')({
+  //   discord: config.discordActual,
+  //   channel: '940290142579605514',
+  //   url: 'https://actualbudget.com/blog/feed.xml'
+  // }),
   'Reddit Actual posts to discord': require('./flows/reddit-posts-post')({
     discord: config.discordActual
   }),
-  'Actual issues->roam/discord': require('./flows/actual-issues-publish')({
-    github: config['github-actual'],
-    roam: actualRoamAuth,
-    discord: config.discordActual
-  }),
+  'Actual server issues->roam/discord': require('./flows/actual-issues-publish')(
+    {
+      name: 'actual-server',
+      github: config['github-actual-server'],
+      discord: config.discordActual
+    }
+  ),
+  'Actual issues->roam/discord': require('./flows/actual-issues-publish')(
+    {
+      name: 'actual',
+      github: config['github-actual'],
+      discord: config.discordActual
+    }
+  ),
   'Actual discussions->discord': require('./flows/actual-discussions-publish')({
     github: config['github-actual'],
     discord: config.discordActual
-  }),
-  'Actual bot': require('./flows/issue-responses')({
-    github: config['github-actual'],
-    botAuth: githubBotAuth
-  }),
-  'Fixed issue': require('./flows/fixed-issue')({
-    githubCode: config['github-code'],
-    botAuth: githubBotAuth
-  }),
-  Release: require('./flows/release')({
-    headerToken: config.release.headerToken,
-    botAuth: githubBotAuth
-  }),
-  'Roam export (actual)': require('./flows/roam-export')({
-    roamAuth: actualRoamAuth,
-    key: 'actual',
-    exportToken: config['actual-roam-export'].token
   })
+  // 'Actual bot': require('./flows/issue-responses')({
+  //   github: config['github-actual'],
+  //   botAuth: githubBotAuth
+  // }),
+  // 'Fixed issue': require('./flows/fixed-issue')({
+  //   githubCode: config['github-code'],
+  //   botAuth: githubBotAuth
+  // }),
+  // Release: require('./flows/release')({
+  //   headerToken: config.release.headerToken,
+  //   botAuth: githubBotAuth
+  // }),
+  // 'Roam export (actual)': require('./flows/roam-export')({
+  //   roamAuth: actualRoamAuth,
+  //   key: 'actual',
+  //   exportToken: config['actual-roam-export'].token
+  // })
 };
 
 async function run() {
